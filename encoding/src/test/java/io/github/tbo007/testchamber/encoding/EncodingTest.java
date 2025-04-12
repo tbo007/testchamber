918c1f6b3176ecfb7d57dbb00ec9e405648ecb7d
danielstein <tbo007@gmx.de>
2025-04-12 11:38:14 +0200

    Encoding: Tika Test

--- encoding/src/test/java/io/github/tbo007/testchamber/encoding/EncodingTest.java
+++ encoding/src/test/java/io/github/tbo007/testchamber/encoding/EncodingTest.java
@@ -2,7 +2,20 @@ package io.github.tbo007.testchamber.encoding;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.tika.detect.CompositeEncodingDetector;
+import org.apache.tika.detect.DefaultDetector;
+import org.apache.tika.detect.Detector;
+import org.apache.tika.detect.EncodingDetector;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.mime.MediaType;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.txt.Icu4jEncodingDetector;
+import org.apache.tika.parser.txt.UniversalEncodingDetector;
+import org.apache.tika.sax.BodyContentHandler;
 import org.junit.jupiter.api.Test;
+import org.xml.sax.SAXException;
 
 import java.io.*;
 import java.nio.charset.Charset;
@@ -10,6 +23,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.Collections;
 
 public class EncodingTest {
 
@@ -19,7 +33,7 @@ public class EncodingTest {
 
     @Test
     void defaultEncoding_BOM () throws IOException {
-        Path path = Path.of("./", "iso885915_withUTF8BOM.txt");
+        Path path = Path.of("./target/", "iso885915_withUTF8BOM.txt");
         try (OutputStream out = Files.newOutputStream(path, StandardOpenOption.WRITE, StandardOpenOption.CREATE,StandardOpenOption.TRUNCATE_EXISTING)) {
 
             int [] unicode_bom = {0xFEFF};
@@ -39,6 +53,7 @@ public class EncodingTest {
         }
     }
 
+    /** log4j2.xml: charset = iso8859-15 **/
     @Test
     void platformEncoding() throws UnsupportedEncodingException {
         String umlautz = "äöü";
@@ -46,9 +61,10 @@ public class EncodingTest {
         String isoString = new String(ISO_8859_15, "ISO-8859-15");
         String utf8String = new String(isoString.getBytes(StandardCharsets.UTF_8), StandardCharsets.UTF_8);
         System.out.println(isoString);
-        log.error("ISO STR: " +isoString);
-        log.error("UTF STR: " +utf8String);
-
+        for (int i = 0; i < 10; i++) {
+            log.error("ISO STR: " +isoString);
+            log.error("UTF STR: " +utf8String);
+        }
         System.out.println(System.getProperty("file.encoding"));
         System.out.println(Charset.availableCharsets());
         System.out.println(Charset.defaultCharset());
@@ -63,18 +79,68 @@ public class EncodingTest {
     }
 
 
-        public static String toHexString(byte[] byteArray) {
-            if (byteArray == null || byteArray.length == 0) {
-                return ""; // Leerer String für leeres oder null-Array
-            }
-
-            StringBuilder hexString = new StringBuilder();
-            for (byte b : byteArray) {
-                // Jedes Byte in einen zweistelligen Hex-Wert umwandeln
-                hexString.append(String.format("%02X", b));
-            }
-            return hexString.toString();
+    public static String toHexString(byte[] byteArray) {
+        if (byteArray == null || byteArray.length == 0) {
+            return ""; // Leerer String für leeres oder null-Array
         }
+
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : byteArray) {
+            // Jedes Byte in einen zweistelligen Hex-Wert umwandeln
+            hexString.append(String.format("%02X", b));
+        }
+        return hexString.toString();
+    }
+
+    @Test
+    void tikaTest2 () throws Exception {
+        String join = String.join("", Collections.nCopies(2000, "ööööaaaa"));
+        ByteArrayInputStream str = new ByteArrayInputStream(join.getBytes("ISO-8859-15"));
+
+ //       try (InputStream input = str) {
+        // log42.log
+        try (FileInputStream input = new FileInputStream("./target/encoding-20250412.log")) {
+            byte [] bytes = new byte[4000];
+            input.read(bytes);
+
+
+            System.out.println(tikaDetect(bytes));
+            String string = new String(bytes,"ISO-8859-15");
+            // Prepend € because it is different in all Three Encodings
+            //string = "€" + string;
+            System.out.println(tikaDetect(string.getBytes("UTF-8")));
+            String winString = string.replaceAll("\\n", "\\r\\n");
+            System.out.println(winString.equals(string));
+            System.out.println(tikaDetect(winString.getBytes("windows-1252")));
+            System.out.println(tikaDetect(string.getBytes("ISO-8859-15")));
+
+            /* Plan:
+             * Eigener Encoding detector
+             * Wenn UTF-X detektiert wird dieses nutzen. Wenn ISO-8859-X oder Windows-12XX detektiert
+             * wird, Strategie aus UniversalEncodingListener nutzen: Wenn \r vorhanden: CP1252
+             * ansonsten 8859-15
+              */
+        }
+    }
+
+    private static Charset tikaDetect(byte[] bytes) throws IOException {
+        Metadata metadata = new Metadata();
+        metadata.set(Metadata.CONTENT_ENCODING,"ISO-8859-15");
+        EncodingDetector detector = new Icu4jEncodingDetector();
+        //EncodingDetector detector = new UniversalEncodingDetector();
+        return detector.detect(new ByteArrayInputStream(bytes), metadata);
+    }
+
+
+    public static String detectDocTypeUsingDetector(InputStream stream)
+            throws IOException {
+        Detector detector = new DefaultDetector();
+        Metadata metadata = new Metadata();
+
+        MediaType mediaType = detector.detect(stream, metadata);
+        return mediaType.toString();
+    }
+
 
 
 
